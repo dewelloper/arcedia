458e102500dba506e3b88249d8bdd6996a5d4c2c
dewelloper <dewelloper@gmail.com>
2017-03-13 15:30:05 +0300

    procfile added

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
-http.createServer(function (req, res) {
+var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
-}).listen(port);
+});//.listen(port);
+
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function () {
+    console.log('Listening on port %d', server_port);
+});
