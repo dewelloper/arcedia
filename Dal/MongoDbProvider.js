38e1f33c885c01bf7cc118adcc6dcb4735455cc9
dewelloper <dewelloper@gmail.com>
2017-03-15 17:20:55 +0300

    mongoDb first implementation added

---
+++ Dal/MongoDbProvider.js
@@ -0,0 +1,24 @@
+﻿// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+// Connect to the db
+//MongoClient.connect("mongodb://localhost:27017/arcediadb", function (err, db) {
+//    if (!err) {
+//        console.log("We are connected");
+//    }
+//});
+
+var URL = "mongodb://localhost:27017/arcediadb";
+
+MongoClient.connect(URL, function (err, db) {
+    if (err) return
+    var results = [];
+    var collection = db.collection('articles');
+    var cursor = collection.find();
+    console.log(cursor);
+    cursor.forEach(function (doc) {
+        results.push(doc);
+        console.log(doc);
+    });
+    console.log("end");
+})
--- server.js
+++ server.js
@@ -7,16 +7,35 @@ var app = express();
 
 //config
 app.set("view engine", 'ejs');
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 app.set("model", path.join(__dirname, "Dal"));
 
 //midleware
 app.use(bodyParser());
 app.use(express.static(path.join(__dirname, "bower_components")));
 
+
 //define routes
 
-app.use(require('./Controllers/MapController'));
+app.use(require('./Controllers/DocController'));
+
+app.get('/', function (req, res) {
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
+
+    res.render('index', {
+        drinks: drinks,
+        tagline: tagline
+    });
+});
+
+app.get('/about', function (req, res) {
+    res.render('about', { title: 'Hey', message: 'Hello there!' })
+});
 
 
 //start the server
