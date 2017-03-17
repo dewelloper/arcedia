5503dd8ae052fabafb865919d27695580d4d3bdb
dewelloper <dewelloper@gmail.com>
2017-03-17 12:09:08 +0300

    index.ejs

--- Controllers/ArticleController.js
+++ Controllers/ArticleController.js
@@ -8,6 +8,26 @@ var promise = require('promise');
 var q = require('q');
 
 
+router.get('/index.ejs', function (req, res) {
+    var articles = [];
+    res.render("index.ejs", { artcls: articles });
+
+    //var articles = [];
+
+    //findAllArticles().then(function (nresult) {
+    //    res.render("index", {artcls: nresult[0] });
+    //});
+
+    //function findAllArticles() {
+    //    var deferred = q.defer();
+    //    arts.find(function (err, docs) {
+    //        articles.push(docs);
+    //        deferred.resolve(articles);
+    //    });
+    //    //setTimeout(function () { alert("Hello"); }, 3000);
+    //    return deferred.promise;
+    //}
+});
 
 router.get('/', function (req, res) {
     var articles = [];
