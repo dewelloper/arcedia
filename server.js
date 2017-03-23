809e1b54731b9524860ab7a6ddd2fd67c751ebae
dewelloper <dewelloper@gmail.com>
2017-03-23 16:16:52 +0300

    node_modules\ejs\lib

--- server.js
+++ server.js
@@ -9,14 +9,17 @@ var app = express();
 app.set("view engine", 'ejs');
 app.set("views", path.join(__dirname, "views"));
 app.set("model", path.join(__dirname, "Dal"));
-
+process.env.PWD = process.cwd();
 
 //midleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, '.\partials')));
-app.use(express.static(path.join(__dirname, '.\node_modules/ejs/lib')));
+//app.use(express.static(path.join(__dirname, '.\partials')));
+//app.use(express.static(path.join(__dirname, '.\node_modules\ejs\lib')));
+
+app.use('/heatcanvas', express.static(process.env.PWD + '/partials'));
+app.use('/heatcanvas', express.static(process.env.PWD + '/node_modules\ejs\lib'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
