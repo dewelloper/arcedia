564c1f22986add9146bee417141c3dbccc2ea9e8
dewelloper <dewelloper@gmail.com>
2017-03-23 17:08:12 +0300

    artart

--- Web.js
+++
@@ -1,4 +0,0 @@
-﻿process.env.PWD = process.cwd();
-
-app.use('/partials', express.static(process.env.PWD + '/partials'));
-app.use('/node_modules/ejs/lib', express.static(process.env.PWD + '/node_modules/ejs/lib'));
