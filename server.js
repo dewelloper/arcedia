c60a990135619154e2b09034c7003cd20c496453
dewelloper <dewelloper@gmail.com>
2017-03-23 15:16:04 +0300

    views .. removed

--- server.js
+++ server.js
@@ -3,33 +3,35 @@ var path = require("path");
 var bodyParser = require("body-parser");
 
 
-
 var app = express();
 
 //config
 app.set("view engine", 'ejs');
-app.set("views", path.join(__dirname, "Views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("model", path.join(__dirname, "Dal"));
 
+
 //midleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "bower_components")));
-app.use(express.static(__dirname + '../public'));
-app.use(express.static(__dirname + '../Views'));
-app.use(express.static(__dirname + '../partials'));
-app.use(express.static(__dirname + '../node_modules/ejs/lib'));
-path.join(__dirname, 'partials', 'head.ejs');
+//app.use(express.static(path.join(__dirname, "bower_components")));
+//app.use(express.static(__dirname + 'partials'));
+//app.use(express.static(__dirname + 'node_modules/ejs/lib'));
 
 
 //define routes
 app.use(require('./Controllers/ArticleController'));
 
+
 app.get('/', function (req, res) {
     var articles = [];
     articles.push({ "id": 1, "name": "json" });
-    const index = path.join(__dirname, 'build', '../Views/index.ejs');
+    const index = path.join(__dirname, 'Views/index.ejs');
     res.render(index, { artcls: articles });
 
     //findAllArticles().then(function (nresult) {
@@ -46,11 +48,7 @@ app.get('/', function (req, res) {
     //}
 });
 
-//start the server
-//var port = process.env.port || 1337;
-//app.listen(port, function (req, res) {
-//    console.log('listening ' + port);
-//});
+
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,2 @@
+/// <reference path="globals/grunt/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
