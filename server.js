9f1d396ee9c33722b4ae2d7d955364444004ec54
Hamit Yıldırım <yildirim.hamit@hotmail.com>
2017-04-04 09:59:23 +0300

    slash

--- server.js
+++ server.js
@@ -5,52 +5,52 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-//config
+\\config
 app.set("view engine", 'ejs');
 app.set("views", path.join(__dirname, "views"));
 app.set("model", path.join(__dirname, "Dal"));
 
 
-//midleware
+\\midleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname, '.\partials')));
-//app.use(express.static(path.join(__dirname, '.\node_modules\ejs\lib')));
+\\app.use(express.static(path.join(__dirname, '.\partials')));
+\\app.use(express.static(path.join(__dirname, '.\node_modules\ejs\lib')));
 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//app.use(express.static(path.join(__dirname, "bower_components")));
-//app.use(express.static(__dirname + 'partials'));
-//app.use(express.static(__dirname + 'node_modules/ejs/lib'));
+\\app.use(express.static(path.join(__dirname, "bower_components")));
+\\app.use(express.static(__dirname + 'partials'));
+\\app.use(express.static(__dirname + 'node_modules\ejs\lib'));
 
 
-//define routes
-app.use(require('./Controllers/ArticleController'));
+\\define routes
+app.use(require('.\Controllers\ArticleController'));
 
 
 
-app.get('/', function (req, res) {
+app.get('\', function (req, res) {
     var articles = [];
     articles.push({ "id": 1, "name": "json" });
-    const index = path.join(__dirname, 'Views/index.ejs');
+    const index = path.join(__dirname, 'Views\index.ejs');
     res.render(index, { artcls: articles });
 
-    //findAllArticles().then(function (nresult) {
-    //    res.render('../Views/index.ejs', { artcls: articles[0] }); 
-    //});
+    \\findAllArticles().then(function (nresult) {
+    \\    res.render('..\Views\index.ejs', { artcls: articles[0] }); 
+    \\});
 
-    //function findAllArticles() {
-    //    var deferred = q.defer();
-    //    arts.find(function (err, docs) {
-    //        articles.push(docs);
-    //        deferred.resolve(articles);
-    //    });
-    //    return deferred.promise;
-    //}
+    \\function findAllArticles() {
+    \\    var deferred = q.defer();
+    \\    arts.find(function (err, docs) {
+    \\        articles.push(docs);
+    \\        deferred.resolve(articles);
+    \\    });
+    \\    return deferred.promise;
+    \\}
 });
 
 
@@ -62,4 +62,4 @@ app.listen(server_port, server_host, function (req, res) {
 });
 
 
-//https://github.com/joemccann/dillinger/issues/514
+\\https:\\github.com\joemccann\dillinger\issues\514
